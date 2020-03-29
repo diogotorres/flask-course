fc84af9acf3f243b9f307566c71e92b723a2aaf4
Diogo Torres <diogo.torres@ymail.com>
2020-03-29 13:22:33 -0300

    SQLAlchemy configurado

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+
+@app.before_first_request
+def create_database():
+    db.create_all()
+
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import db
+    db.init_app(app)
     app.run(debug=True)
